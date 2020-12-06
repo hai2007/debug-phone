b04c6274f50f9de2b5800f45e9d6529f1edc98d5
hai2007 <2501482523@qq.com>
2020-12-06 13:30:58 +0800

    恢复基于@hai2007/tool的类型判断

--- src/services/showData.js
+++ src/services/showData.js
@@ -1,15 +1,5 @@
 import xhtml from 'xhtml.js';
-
-let isObject = function (value) {
-    const type = typeof value;
-    return value != null && (type === 'object' || type === 'function');
-};
-
-let isString = function (value) {
-    if (value == null) return false;
-    const type = typeof value;
-    return type === 'string' || (type === 'object' && value != null && !Array.isArray(value) && Object.prototype.toString.call(value) === '[object String]');
-};
+import { isString, isFunction, isUndefined, isNull, isNumber, isBoolean } from '@hai2007/tool/type';
 
 let doit = (target, obj) => {
 
@@ -57,10 +47,16 @@ export default function (target, ol, data) {
 
         let li = target.$document.createElement('li');
 
-        if (undefined === data[i]) li.innerText = 'undefined';
-        else if (null === data[i]) li.innerText = 'null';
 
-        else if (isObject(data[i])) {
+        // 如果是字符串、函数、数字等
+        if (isString(data[i]) || isFunction(data[i]) || isNumber(data[i]) || isBoolean(data[i])) {
+            li.innerText = data[i];
+        }
+
+        else if (isUndefined(data[i])) li.innerText = 'undefined';
+        else if (isNull(data[i])) li.innerText = 'null';
+
+        else {
 
             // 默认作为对象显示
             li.setAttribute('hadload', 'no');
@@ -68,8 +64,6 @@ export default function (target, ol, data) {
             li.innerHTML = `<span>${data[i]}</span>`;
             doit(li, data[i]);
 
-        } else {
-            li.innerText = data[i];
         }
 
         ol.appendChild(li);
